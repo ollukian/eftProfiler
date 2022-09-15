b6958faca78c2f4f1f41619082f6a09084f889cb
ollukian <77246672+ollukian@users.noreply.github.com>
2022-09-15 21:06:11 +0200

    Update for CMake 3.12 support

--- job_script.sh
+++ job_script.sh
@@ -2,4 +2,4 @@ echo "[INFO] run script..."
 echo "[INFO] run executable.."
 #singularity exec --bind /cvmfs -H "$PWD" /cvmfs/unpacked.cern.ch/gitlab-registry.cern.ch/atlas_higgs_combination/software/hcomb-docker/analyzer:2-2 sh build.sh $@
 #singularity exec --bind /cvmfs -H "$PWD" /cvmfs/unpacked.cern.ch/gitlab-registry.cern.ch/atlas_higgs_combination/software/hcomb-docker/analyzer\:latest sh build.sh $@
-singularity exec --bind /cvmfs -H "$PWD" /cvmfs/unpacked.cern.ch/gitlab-registry.cern.ch/atlas_higgs_combination/software/hcomb-docker/analyzer\:analyzer:paper2021-6 sh build.sh $@
+singularity exec --bind /cvmfs -H "$PWD" /cvmfs/unpacked.cern.ch/gitlab-registry.cern.ch/atlas_higgs_combination/software/hcomb-docker/analyzer\:paper2021-6 sh build.sh $@
