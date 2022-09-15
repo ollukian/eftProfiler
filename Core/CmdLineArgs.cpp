ce3530ed515c9de1cb5e91bd25bdf11c5c29c2cf
ollukian <77246672+ollukian@users.noreply.github.com>
2022-09-15 14:35:41 +0200

    Debug CmdLineArgs.cpp

--- Core/CmdLineArgs.cpp
+++ Core/CmdLineArgs.cpp
@@ -24,26 +24,54 @@ CmdLineArgs::CmdLineArgs(int argc, char **argv) {
 
 bool CmdLineArgs::ParseInput(int argc, char* argv[])
 {
+    cout << "create big line:" << endl;
+    string args;
+
     for (size_t idx {1}; idx != argc; ++idx) {
-        string arg = {argv[idx]};
+        args += " " + string(argv[idx]);
+    }
 
-        if (arg.find('-') != string::npos) {
-            if (idx < argc) {
-                string new_args = argv[++idx];
-                while (arg.find('-') != string::npos) {
-                    arg += ' ' + new_args;
-                    new_args = argv[++idx];
-                } // extracted all relevant args
-            }
-        }
+    cout << "total line: {" << args << "}" << endl;
 
-        cout << fmt::format("\t{}", arg) << endl;
-
-        auto [key, vals] = ExtractVals(std::move(arg));
+    while ( ! args.empty() ) {
+        size_t idx_first_arg_begin = args.find_first_of('-');
+        size_t idx_last_arg_begin  = args.find_first_not_of('-');
+        string arg_line = args.substr(idx_first_arg_begin, idx_last_arg_begin - idx_first_arg_begin);
+        args = args.substr(idx_last_arg_begin, args.size());
+        cout << "to parse: {" << arg_line << "}" << endl;
+        auto [key, vals] = ExtractVals(std::move(arg_line));
         keys.insert(key);
-        //keys.push_back(key);
+//        //keys.push_back(key);
         ops[key] = std::move(vals);
     }
+
+    //for (size_t idx {1}; idx != argc; ++idx) {
+        //cout << "parse input. Idx: " << idx << endl;
+        //string arg = {argv[idx]};
+        //cout << "arg: " << arg << endl;
+
+//        if (arg.find('-') != string::npos) {
+//            cout << "arg contains - symbol" << endl;
+//            if (idx < argc - 1) {
+//                cout << "* can increase idx" << endl;
+//                string new_args = argv[++idx];
+//                cout << "* new args: " << new_args << endl;
+//                while (arg.find('-') == string::npos) {
+//                    cout << " ** keep adding";
+//                    arg += ' ' + new_args;
+//                    new_args = argv[++idx];
+//                    cout << "arg: {" << arg << "}, new_args: {" << new_args << "}" << endl;
+//                } // extracted all relevant args
+//            }
+//        }
+
+       // cout << fmt::format("\t{}", arg) << endl;
+//
+//        auto [key, vals] = ExtractVals(std::move(arg));
+//        keys.insert(key);
+//        //keys.push_back(key);
+//        ops[key] = std::move(vals);
+//    }
     return true;
 }
 
