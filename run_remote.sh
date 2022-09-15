1784ea4aab3e99a745ed50746ea4fa2c8fb5b0d2
ollukian <77246672+ollukian@users.noreply.github.com>
2022-09-15 15:21:51 +0200

    Update run scripts

--- run_remote.sh
+++ run_remote.sh
@@ -6,11 +6,13 @@ submit_one_worker()
 
 
   echo "source ${current_script}"
+  sbatch --mail-user lukianchuk@lal.in2p3.fr --mail-type=END,FAIL -L sps --mem 8G --export=ALL --job-name j_np_${worker_id} --output /sps/atlas/o/ollukian/scratch/Log_np_${worker_id}.OU --error /sps/atlas/o/ollukian/scratch/Log_np_${worker_id}.ER ${current_script}
+
   #qsub -P P_atlas -m e -M lukianchuk@lal.in2p3.fr -l h_rss=4G -l h_cpu=8:00:00 -l vmem=8G,fsize=8G,sps=1 -N j_np_.${worker_id} -o /sps/atlas/o/ollukian/scratch/Log_np_${worker_id}.OU -e /sps/atlas/o/ollukian/scratch/Log_np_${worker_id}.ER ${current_script}
   sleep 1
 }
 
-for worker_id in {0..2}
+for worker_id in {0..4}
 do
   submit_one_worker
 done
