8dc5af2bdb58f8f003ffc51b9c893395154434f6
ollukian <77246672+ollukian@users.noreply.github.com>
2022-09-22 23:20:13 +0200

    Debug free fit

--- Application/FitManager.h
+++ Application/FitManager.h
@@ -253,7 +253,8 @@ inline void FitManager::SetAllGlobObsTo(float val) noexcept
         EFT_PROF_DEBUG("[FitManager][SetAllGlobObsTo] val before: {}",
                        dynamic_cast<RooRealVar *>(globObs)->getVal());
         dynamic_cast<RooRealVar *>(globObs)->setVal(val);
-        EFT_PROF_DEBUG("[FitManager][SetAllGlobObsTo], status after: {} +- {}  (const? ==> {})", name,
+        EFT_PROF_DEBUG("[FitManager][SetAllGlobObsTo] status of {:30} after: {} +- {}  (const? ==> {})",
+                       name,
                        dynamic_cast<RooRealVar *>(globObs)->getVal(),
                        dynamic_cast<RooRealVar *>(globObs)->getError(),
                        dynamic_cast<RooRealVar *>(globObs)->isConstant());
