631f5c521c04a9bb836640769be41ae466b17245
ollukian <77246672+ollukian@users.noreply.github.com>
2022-11-11 19:56:20 +0100

    Try using Hesse only once

--- Fitter/Fitter.cpp
+++ Fitter/Fitter.cpp
@@ -80,12 +80,48 @@ IFitter::FitResPtr Fitter::Minimize(const FitSettings& settings) {
       minim.save("hesse","")->Print();
       }*/
 
+    switch (settings.errors) {
+        case Errors::HESSE:
+            EFT_PROF_INFO("[Minimizer] start HESSE...");
+            minim.hesse();
+            EFT_PROF_INFO("[Minimizer] HESSE is done");
+            //minim.save("hesse", "")->Print();
+            break; // nothing
+        case Errors::MINOS_NPS:
+            assert(settings.nps);
+            EFT_PROF_INFO("[Minimizer] re-estimate errors for NPS");
+            minim.minos(*settings.nps);
+            break;
+        case Errors::MINOS_POIS:
+            EFT_PROF_INFO("[Minimizer] re-estimate errors for POIS");
+            assert(settings.pois);
+            minim.minos(*settings.pois);
+            break;
+        case Errors::MINOS_ALL:
+            EFT_PROF_INFO("[Minimizer] re-estimate errors for NPS & POIS");
+            assert(settings.nps);
+            assert(settings.pois);
+            minim.minos(RooArgSet{*settings.pois, *settings.nps});
+            break;
+        case Errors::DEFAULT:
+            EFT_PROF_INFO("[Minimizer] no need to re-estimate errors, default strategy is set");
+            break;
+    }
+
+//    if (settings.errors == Errors::HESSE) {
+//        EFT_PROF_INFO("[Minimizer] start HESSE...");
+//        minim.hesse();
+//        EFT_PROF_INFO("[Minimizer] HESSE is done");
+//        //minim.save("hesse", "")->Print();
+//    }
+
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     /// COPIED from QuickFit:                                                                                        //
     // https://gitlab.cern.ch/atlas_higgs_combination/projects/lhc-comb-tools/-/blob/master/quickFit/src/fitTool.cxx //
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // Copied from RooAbsPdf::fitTo()
     //if (_doSumW2==1 && minim.getNPar()>0) {
+#if 0
     if (settings.errors != Errors::DEFAULT) {
     //if (false) {
 
@@ -93,8 +129,10 @@ IFitter::FitResPtr Fitter::Minimize(const FitSettings& settings) {
             EFT_PROF_INFO("[Minimizer] start HESSE...");
             minim.hesse();
             EFT_PROF_INFO("[Minimizer] HESSE is done");
-            minim.save("hesse", "")->Print();
+            //minim.save("hesse", "")->Print();
         }
+
+
 
         EFT_PROF_INFO("[Minimizer] Evaluating SumW2 error...");
         // Make list of RooNLLVar components of FCN
@@ -158,7 +196,7 @@ IFitter::FitResPtr Fitter::Minimize(const FitSettings& settings) {
 
         switch (settings.errors) {
             case Errors::HESSE:
-                EFT_PROF_INFO("[Minimizer] no need to re-estimate errors");
+                EFT_PROF_INFO("[Minimizer] no need to re-estimate errors with MINOS: already done Hesse");
                 break; // nothing
             case Errors::MINOS_NPS:
                 assert(settings.nps);
@@ -177,12 +215,13 @@ IFitter::FitResPtr Fitter::Minimize(const FitSettings& settings) {
                 minim.minos(RooArgSet{*settings.pois, *settings.nps});
                 break;
             case Errors::DEFAULT:
-                EFT_PROF_INFO("[Minimizer] no need to re-estimate errors");
+                EFT_PROF_INFO("[Minimizer] no need to re-estimate errors, default strategy is set");
                 break;
         }
 
 
     }
+#endif
 
     auto result = make_unique<RooFitResult>(
             *minim.save("fitResult","Fit Results")
