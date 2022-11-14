1eb55ef8e2ea81892d5e1a238fef76e9830ce15b
ollukian <77246672+ollukian@users.noreply.github.com>
2022-11-14 14:29:10 +0100

    Upd submit script. Add poi support

--- run_remote.sh
+++ run_remote.sh
@@ -6,7 +6,7 @@ submit_one_worker()
   echo "# script automatically created by run_remote version @ 14Nov22 / Lukianchuk Aleksei lukianchuk.aleksei@gmail.com" >> "${current_script}"
   echo "# POI: ${poi}, worker_id: ${worker_id}" >> "${current_script}"
   echo "cd /pbs/home/o/ollukian/public/EFT/git/eftProfiler" >> "${current_script}"
-  echo "sh job_script.sh --task compute_ranking --errors Hesse --poi_init_val 0 --worker_id ${worker_id} --poi ${poi} --res_path /pbs/home/o/ollukian/public/EFT/git/eftProfiler/res_${poi}/ --no_gamma" >> "${current_script}"
+  echo "sh job_script.sh --task compute_ranking --errors Hesse --poi_init_val 0 --worker_id ${worker_id} --poi ${poi} --res_path /pbs/home/o/ollukian/public/EFT/git/eftProfiler/res_${poi}/ --no_gamma --ws_path ${ws_path}" >> "${current_script}"
 
   echo "source ${current_script}"
   sbatch --mail-user lukianchuk@lal.in2p3.fr --mail-type=END,FAIL -L sps --mem 20G --export=ALL --job-name "${poi}_${worker_id}" --output "/sps/atlas/o/ollukian/scratch/Log_${poi}_${worker_id}.OU" --error "/sps/atlas/o/ollukian/scratch/Log_${poi}_${worker_id}.ER" "${current_script}"
@@ -14,7 +14,7 @@ submit_one_worker()
   sleep 10
 }
 
-
+ws_path=../source/WS-Comb-EFT-fitbasis-Asimov_v3.root
 poi=eHRe33
 for worker_id in {0..20}
 do
