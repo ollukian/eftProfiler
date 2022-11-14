0f76be6af48ab31d8c69f4e144512e86584cf5cf
ollukian <77246672+ollukian@users.noreply.github.com>
2022-11-14 14:01:06 +0100

    Update running scripts. Add run with singularity

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 echo "[INFO] run code with arguments: {$@} "
+cd build  || echo "[ERROR] cannot cd to the build directory, we're at: {$PWD}" exit
 ./eftProfiler $@
 echo "clean up: delete dump of the linux kernel (core.* files)"
-cd build || echo "[ERROR] cannot cd to the build directory, we're at: {$PWD}" exit
+# cd build || echo "[ERROR] cannot cd to the build directory, we're at: {$PWD}" exit
 rm core.*
 cd ..
 echo "[INFO] run has successfully finished {$@} "
