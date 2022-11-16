2c266a7e5831ce16e85d33f69f069c5ac62d02cd
ollukian <77246672+ollukian@users.noreply.github.com>
2022-11-16 15:12:30 +0100

    Revise Callback printing for Selector

--- Utils/NpRankingPlotter.h
+++ Utils/NpRankingPlotter.h
@@ -48,7 +48,7 @@ private:
     static std::shared_ptr<TH1D>    MakeHisto1D(const std::string& name, size_t nb_bins) noexcept;
 
     static inline EntriesSelector   CreateLambdaForIgnoringNpNames(const std::vector<std::string>& names_to_ignore) noexcept;
-    static inline EntriesSelector   CreateLambdaForMatchingNpNames(const std::vector<std::string>& names_to_ignore) noexcept;
+    static inline EntriesSelector   CreateLambdaForMatchingNpNames(const std::vector<std::string>& names_to_match) noexcept;
 private:
     EntriesSelector callback_ {[](const NpInfoForPlot&){return true;}};
     std::unordered_map<std::string, NpRankingStudyRes> np_study_res_;
@@ -73,20 +73,23 @@ public:
 NpRankingPlotter::EntriesSelector
 NpRankingPlotter::CreateLambdaForIgnoringNpNames(const std::vector<std::string>& names_to_ignore) noexcept {
     return [&](const NpInfoForPlot& info) -> bool {
-        return std::all_of(names_to_ignore.begin(), names_to_ignore.end(), [&](const auto& name) -> bool
+        bool res =  std::all_of(names_to_ignore.begin(), names_to_ignore.end(), [&](const auto& name) -> bool
         {
             return (info.name.find(name) == std::string::npos);
         });
+        EFT_PROF_DEBUG("callback [name ignore] for POI: {:10}, np: {:20} result: {}", info.poi, info.name, res);
     };
 }
 
 NpRankingPlotter::EntriesSelector
-NpRankingPlotter::CreateLambdaForMatchingNpNames(const std::vector<std::string>& names_to_ignore) noexcept {
+NpRankingPlotter::CreateLambdaForMatchingNpNames(const std::vector<std::string>& names_to_match) noexcept {
     return [&](const NpInfoForPlot& info) -> bool {
-        return std::all_of(names_to_ignore.begin(), names_to_ignore.end(), [&](const auto& name) -> bool
+        bool res = std::all_of(names_to_match.begin(), names_to_match.end(), [&](const auto& name) -> bool
         {
             return (info.name.find(name) != std::string::npos);
         });
+        EFT_PROF_DEBUG("callback [name match] for POI: {:10}, np: {:20} result: {}", info.poi, info.name, res);
+        return res;
     };
 }
 
