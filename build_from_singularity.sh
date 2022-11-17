0dd74efc1c1cae41db229f58409a2a751ba8aee0
ollukian <77246672+ollukian@users.noreply.github.com>
2022-11-17 10:49:51 +0100

    Fix script for building from singularity

--- build_from_singularity.sh
+++ build_from_singularity.sh
@@ -1,4 +1,4 @@
 echo "[INFO] build from singularity"
-sh run_from_singularity build.sh || echo "[ERROR] building from singularity"; exit
+sh run_from_singularity.sh build.sh || echo "[ERROR] building from singularity"; exit
 
 echo "[INFO] successfully built from singularity"
