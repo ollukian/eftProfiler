6873e28fee048176ed0c6f6d1c6d34cfe019244c
ollukian <77246672+ollukian@users.noreply.github.com>
2022-11-21 15:15:30 +0100

    Debug cmdline tests

--- Test/TestCommandLineArguments.cpp
+++ Test/TestCommandLineArguments.cpp
@@ -21,9 +21,10 @@ void ConvertToArgcAgv(istringstream& s, int& argc, char** argv)
     argv = new char* [argc];
     for (size_t idx {}; static_cast<int>(idx) < argc; ++idx) {
         EFT_PROF_DEBUG(" try initiate argv[{}] = {}", idx, components[idx]);
-        argv[idx] = new char [components[idx].size() - 1];
+        argv[idx] = new char [components[idx].size()];
         for (size_t idx_inner {0}; idx_inner < components[idx].size() - 1; ++idx_inner) {
-            argv[idx][idx_inner] = components[idx][idx_inner];
+            EFT_PROF_INFO("assign to argv[{}][{}] = {}", idx, idx_inner, components[idx][idx_inner]);
+            argv[idx][idx_inner] = static_cast<char>(components[idx][idx_inner]); //components[idx][idx_inner];
         }
         //memcpy(argv[idx], components[idx].data(), components[idx].size());
 
