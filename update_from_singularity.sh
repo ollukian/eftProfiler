50945a7eeb60b75c6d7a64496ee0dc260ec3d5e3
ollukian <77246672+ollukian@users.noreply.github.com>
2022-11-22 11:45:04 +0100

    Debugging get

--- update_from_singularity.sh
+++ update_from_singularity.sh
@@ -1,8 +1,11 @@
 echo "[INFO] update script: update from git & test"
 echo "[INFO] update..."
-git pull  && echo "updated then what?"|| echo "[ERROR] in git pull"; exit;
-echo "[INFO] build..."
-sh wrap_by_singularity.sh sh build.sh || echo "[ERROR] in building"; exit;
-echo "[INFO] test..."
-sh wrap_by_singularity.sh sh run.sh --test || echo "[ERROR] running tests"; exit;
-echo "[INFO] successfully updated"
+#git pull  && echo "updated then what?"|| echo "[ERROR] in git pull"; exit;
+
+sh wrap_by_singularity sh update.sh || echo "[ERROR] updating"
+
+#echo "[INFO] build..."
+#sh wrap_by_singularity.sh sh build.sh || echo "[ERROR] in building"; exit;
+#echo "[INFO] test..."
+#sh wrap_by_singularity.sh sh run.sh --test || echo "[ERROR] running tests"; exit;
+#echo "[INFO] successfully updated"
