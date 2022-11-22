61666d97e32b4974b48076d0af684af2f802f94f
ollukian <77246672+ollukian@users.noreply.github.com>
2022-11-22 12:38:46 +0100

    Update scripts

--- run_from_singularity.sh
+++ run_from_singularity.sh
@@ -1,5 +1,5 @@
-echo "[INFO] run from singularity with args" ${@}
+echo "[INFO] run from singularity with args" "$*"
 
-sh wrap_by_singularity.sh sh run.sh $@ || echo "[ERROR] running form singularity"; exit #1;
-echo "[INFO] successfully ran from singularity with args: " ${@}
+sh wrap_by_singularity.sh sh run.sh "$*" || echo "[ERROR] running form singularity"; exit; #1;
+echo "[INFO] successfully ran from singularity with args: " "$*"
 #exit 0;
