dc5a803a1c44be107b9981e1e06594d0dc0cbbbc
ollukian <77246672+ollukian@users.noreply.github.com>
2022-11-22 12:43:38 +0100

    Update scripts

--- update.sh
+++ update.sh
@@ -2,11 +2,9 @@ echo "[INFO] update script: build & test"
 #echo "[INFO] update..."
 #git pull || echo "[ERROR] in git pull"; exit;
 
-
-
 echo "[INFO] build..."
-sh build.sh || echo "[ERROR] in building"; exit #1;
+sh build.sh || echo "[ERROR] in building" exit #1;
 echo "[INFO] test..."
-sh run.sh --test || echo "[ERROR] running tests"; exit #1;
+sh run.sh --test || echo "[ERROR] running tests" exit #1;
 echo "[INFO] successfully updated"
 exit #0;
