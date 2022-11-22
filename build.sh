328b838517c31b5790b79caced80c4eb7bab155f
ollukian <77246672+ollukian@users.noreply.github.com>
2022-11-22 11:52:45 +0100

    Update scripts

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ echo "[INFO] create build directory if it doesn't exist and cd there.. "
 mkdir -p build && cd build || echo "[ERROR] in cd build "; exit 1
 echo "[INFO] Current path: $PWD "
 echo "[INFO] cmake $PWD.."
-cmake .. || echo "[ERROR] in cmake "; exit 1
+cmake .. || echo "[ERROR] in cmake "; exit #1
 echo "[INFO] make -j"
-make -j ||  echo "[ERROR] in make "; exit 1
+make -j ||  echo "[ERROR] in make "; exit #1
 echo "[INFO] binary has been successfully built"
-exit 0
+#exit 0
