446fc3509f3fcb3a31312f6b5f992d14791578a3
ollukian <77246672+ollukian@users.noreply.github.com>
2022-11-22 11:53:21 +0100

    Update scripts

--- build_from_singularity.sh
+++ build_from_singularity.sh
@@ -2,4 +2,4 @@ echo "[INFO] build from singularity"
 sh wrap_by_singularity.sh sh build.sh || echo "[ERROR] building from singularity"; exit 1;
 
 echo "[INFO] successfully built from singularity"
-exit 0
+#exit 0
