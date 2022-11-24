4241d02efe53b855a1648eedcbd191a7519d75fb
ollukian <77246672+ollukian@users.noreply.github.com>
2022-11-24 20:06:36 +0100

    Adding printing of elapsed time

--- Test/Tester.cpp
+++ Test/Tester.cpp
@@ -23,9 +23,9 @@ void Tester::RunTests(const std::string& groupname_to_run_only)
 
     Logger::GetLogger()->set_level(spdlog::level::info);
     EFT_PROF_INFO("Run all tests");
-    fmt::print(cout, "=={:=<45}==={:=<10}==\n", "", "");
-    fmt::print(cout, "| {:^45} | {:^10} | {:10} |\n", "Test Name", "Status", "Elapsed Time");
-    fmt::print(cout, "=={:=<45}==={:=<10}==\n", "", "");
+    fmt::print(cout, "=={:=<45}==={:=<10}==={:=<10}==\n", "", "");
+    fmt::print(cout, "| {:^45} | {:^10} | {:10} |\n", "Test Name", "Status", "Duration");
+    fmt::print(cout, "=={:=<45}==={:=<10}==={:=<10}==\n", "", "");
     //EFT_PROF_INFO("=={:=<45}==={:=<10}==", "", "");
     //EFT_PROF_INFO("| {:^45} | {:^10} |", "Test Name", "Status");
     //EFT_PROF_INFO("=={:=<45}==={:=<10}==", "", "");
@@ -42,7 +42,7 @@ void Tester::RunTests(const std::string& groupname_to_run_only)
         //}
         bool group_ok = true;
         fmt::print(cout, "| {:^58} |\n", groupname);
-        fmt::print(cout, "+{:-<45}--+-{:-<10}-+\n", "", "");
+        fmt::print(cout, "+{:-<45}--+-{:-<10}--+-{:-<10}-+\n", "", "");
         //EFT_PROF_INFO("| {:^58} |", groupname);
         //EFT_PROF_INFO("+{:-<45}--+-{:-<10}-+", "", "");
         for (const auto& [name, function] : tests)
@@ -58,7 +58,9 @@ void Tester::RunTests(const std::string& groupname_to_run_only)
             }
             else {
                 group_ok = false;
-                fmt::print(fmt::fg(fmt::color::red), "| {:45} | {:10} | {:10} |\n", name, test_res.res, test_res.duration.count());
+                ////fmt::print(fmt::fg(fmt::color::red), "| {:45} | {:10} | {:10} |\n", name, test_res.res, test_res.duration.count());
+                fmt::print(fmt::fg(fmt::color::red), "| {:45} | fail | {:10} |\n", name,  test_res.duration.count());
+                fmt::print(fmt::fg(fmt::color::red), "| {}", test_res.res);
                 //EFT_PROF_ERROR("| {:45} | {:10} |", name, test_res);
                 //fmt::print(cerr, "| {:45} | {:10} |\n", name, std::move(test_res));
             }
@@ -66,12 +68,12 @@ void Tester::RunTests(const std::string& groupname_to_run_only)
             Logger::SetLevel(spdlog::level::level_enum::info);
         } // tests in this group
         if (tr_.GetFailCount() == 0) {
-            fmt::print(cout, "+{:-<45}--+-{:-<10}-+\n", "-", "-");
+            fmt::print(cout, "+{:-<45}--+-{:-<10}}--+-{:-<10}-+\n", "-", "-");
             //EFT_PROF_INFO("+{:-<45}--+-{:-<10}-+", "-", "-");
             //EFT_PROF_INFO("Tests Group: {:30} all test have been successfully passed", groupname);
         }
         else {
-            fmt::print(fmt::fg(fmt::color::red), "Tests Group: {:30}, {} tests have failed\n", groupname, tr_.GetFailCount());
+            fmt::print(fmt::fg(fmt::color::red), "| Tests Group: {:30}, {} tests have failed\n", groupname, tr_.GetFailCount());
             //fmt::print(cerr, "Tests Group: {:30}, {} tests have failed\n", groupname, tr_.GetFailCount());
             //EFT_PROF_CRITICAL("Tests Group: {:30}, {} tests have failed", groupname, tr_.GetFailCount());
             return;
