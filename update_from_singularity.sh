7a02d1c03680c8b4c5944d93f5e7c75cb5221312
ollukian <77246672+ollukian@users.noreply.github.com>
2022-11-24 13:11:38 +0100

    Upd update scripts: exit in case of git pull error

--- update_from_singularity.sh
+++ update_from_singularity.sh
@@ -5,14 +5,14 @@ changed=0
 git remote update && git status -uno | grep -q 'Your branch is behind' && changed=1
 if [ $changed = 1 ]; then
     echo "[INFO] update is required, update..."
-    can_be_pulled=0
-    git pull | grep -q 'Your local changes to the following files would be overwritten by merge' && can_be_pulled=1
-    if [ $can_be_pulled = 0 ]; then
+    #can_be_pulled=0
+    git pull #| grep -q 'Your local changes to the following files would be overwritten by merge' && can_be_pulled=1
+    #if [ $can_be_pulled = 0 ]; then
       echo "[INFO] build...";
       sh wrap_by_singularity.sh sh build_and_test.sh || echo "[ERROR] in build&test"; exit #1;
-    else
-      echo "[ERROR] pulling is not possible, check output of git pull manually"; exit
-    fi
+    #else
+     # echo "[ERROR] pulling is not possible, check output of git pull manually"; exit
+    #fi
 else
     echo "[INFO] update is NOT required"
 fi
