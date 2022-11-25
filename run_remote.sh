af01fd2912d0391b1c50183573d28217c8121afb
ollukian <77246672+ollukian@users.noreply.github.com>
2022-11-25 14:49:17 +0100

    Upd submission script

--- run_remote.sh
+++ run_remote.sh
@@ -4,7 +4,8 @@ submit_one_worker()
   current_script=/sps/atlas/o/ollukian/scratch/script_ranking_${POI}_${worker_id}.sh
   echo "#!/bin/sh" > "${current_script}"
   echo "####################################################################################################################" >> "${current_script}"
-  echo "# script automatically created by run_remote version @ 16Nov22 / Lukianchuk Aleksei lukianchuk.aleksei@gmail.com  ##" >> "${current_script}"
+  echo "# script automatically created by run_remote version @ 25Nov22 / Lukianchuk Aleksei lukianchuk.aleksei@gmail.com  ##" >> "${current_script}"
+  echo "# eftProfiler version 0.9.0 @ 25Nov22                                                                              ##" >> "${current_script}"
   echo "####################################################################################################################" >> "${current_script}"
   echo "# script is called from: ${PWD} with the following settings:" >> "${current_script}"
   echo "# save path to:    ${EFT_SAVE_RES_TO}"                        >> "${current_script}"
@@ -16,7 +17,7 @@ submit_one_worker()
   echo "##################################################################################################################" >> "${current_script}"
 
   echo "cd ${EFT_PROFILER_PATH}" >> "${current_script}"
-  echo "sh job_script.sh --task compute_ranking --errors ${EFT_ERRORS_HANDLING} --poi_init_val 0 --worker_id ${worker_id} --poi ${POI} --res_path ${EFT_SAVE_RES_TO} --no_gamma --ws_path ${WS_PATH}" >> "${current_script}"
+  echo "sh job_script.sh --task compute_ranking --errors ${EFT_ERRORS_HANDLING} --poi_init_val ${POI_INIT_VAL} --worker_id ${worker_id} --poi ${POI} --res_path ${EFT_SAVE_RES_TO} --no_gamma --ws_path ${WS_PATH}" >> "${current_script}"
 
   echo "source ${current_script}"
   sbatch --mail-user lukianchuk@lal.in2p3.fr --mail-type=END,FAIL -L sps --mem 20G --export=ALL --job-name "${worker_id}${POI}" --output "/sps/atlas/o/ollukian/scratch/Log_${POI}_${worker_id}.OU" --error "/sps/atlas/o/ollukian/scratch/Log_${POI}_${worker_id}.ER" "${current_script}"
@@ -27,6 +28,7 @@ submit_one_worker()
 WS_NAME="WS-Comb-Higgs_topU3l_obs.root"
 POI=cHG
 
+POI_INIT_VAL=0
 EFT_PROFILER_PATH="${PWD}"
 WS_PATH=${EFT_PROFILER_PATH}/source/${WS_NAME}
 EFT_SAVE_RES_TO=${EFT_PROFILER_PATH}/res__${POI}
