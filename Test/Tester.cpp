0fc44e6eb083a4403e69e599d86343f04604597c
ollukian <77246672+ollukian@users.noreply.github.com>
2022-11-25 09:24:31 +0100

    Change order of tests: ws wrapper after cmd line args

--- Test/Tester.cpp
+++ Test/Tester.cpp
@@ -92,6 +92,6 @@ void Tester::InitSetTests() {
     EFT_RUN_TESTFILE(StringUtils);
     EFT_RUN_TESTFILE(ColourUtils);
     EFT_RUN_TESTFILE(PlotterUtils);
-    EFT_RUN_TESTFILE(WorkSpaceWrapper);
     EFT_RUN_TESTFILE(CommandLineArguments); // problems with vec<string> ==> argv convertion
+    EFT_RUN_TESTFILE(WorkSpaceWrapper);
 }
