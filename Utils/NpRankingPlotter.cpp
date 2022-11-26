e4ca7f49458365d596e0bec54ee9b99a8fbed008
ollukian <77246672+ollukian@users.noreply.github.com>
2022-11-26 01:16:23 +0100

    Revise vertical plotting

--- Utils/NpRankingPlotter.cpp
+++ Utils/NpRankingPlotter.cpp
@@ -568,14 +568,14 @@ namespace eft::plot {
         TBox marker_posfit_minus {x_start += dx_between_markers, settings->rmuh * y_start_multiplier,  x_start += x_size_one_block, settings->rmuh * y_end_multiplier};
 
 
-        //marker_prefit_plus.SetLineColor(settings->color_prefit_plus);
-        //marker_prefit_minus.SetLineColor(settings->color_prefit_minus);
+        marker_prefit_plus.SetLineColor(settings->color_prefit_plus);
+        marker_prefit_minus.SetLineColor(settings->color_prefit_minus);
 
-        marker_prefit_plus.SetFillColorAlpha(settings->color_prefit_plus,
-                                             utils::ColourUtils::GetColourByIdx(
-                                                     settings->color_prefit_plus)
-                                                     .a_as_fraction()
-        );
+//        marker_prefit_plus.SetFillColorAlpha(settings->color_prefit_plus,
+//                                             utils::ColourUtils::GetColourByIdx(
+//                                                     settings->color_prefit_plus)
+//                                                     .a_as_fraction()
+//        );
 
         marker_prefit_minus.SetFillColorAlpha(settings->color_prefit_minus,
                                              utils::ColourUtils::GetColourByIdx(
@@ -583,11 +583,11 @@ namespace eft::plot {
                                                      .a_as_fraction()
         );
 
-        marker_prefit_plus.SetLineWidth(3);
-        marker_prefit_minus.SetLineWidth(3);
+        marker_posfit_plus.SetLineWidth(1);
+        marker_posfit_plus.SetLineWidth(1);
 
-        marker_prefit_plus.SetFillStyle(0);
-        marker_prefit_minus.SetFillStyle(0);
+        marker_posfit_plus.SetFillStyle(0);
+        marker_posfit_plus.SetFillStyle(0);
 
         marker_posfit_plus.SetFillColorAlpha(settings->color_postfit_plus,
                                              utils::ColourUtils::GetColourByIdx(
