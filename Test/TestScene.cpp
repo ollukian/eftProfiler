bf616140c6fe9e9e244684bbe218707e4d533081
ollukian <77246672+ollukian@users.noreply.github.com>
2022-11-28 13:07:52 +0100

    Add tests for scene

--- Test/TestScene.cpp
+++ Test/TestScene.cpp
@@ -13,14 +13,88 @@
 using namespace std;
 using namespace eft::utils::draw;
 
+void TestDrawableCtorFromUniquePtr() {
+    {
+        auto box = make_unique<TBox>(0, 0, 1, 1);
+        ASSERT_NO_THROW(Drawable::Create(std::move(box)));
+    }
+    {
+        auto box = make_unique<TBox>(0, 0, 1, 1);
+        auto box_drawable = Drawable::Create(std::move(box));
+        ASSERT(box_drawable.get());
+        ASSERT_EQUAL(box_drawable->name, "");
+        ASSERT_EQUAL(box_drawable->draw_options, "");
+        ASSERT_EQUAL(box_drawable->should_be_drawn, true);
+    }
+    {
+        auto box = make_unique<TBox>(0, 0, 1, 1);
+        auto box_drawable = Drawable::Create(std::move(box), "name_box");
+        ASSERT(box_drawable.get());
+        ASSERT_EQUAL(box_drawable->name, "name_box");
+        ASSERT_EQUAL(box_drawable->draw_options, "");
+        ASSERT_EQUAL(box_drawable->should_be_drawn, true);
+    }
+    {
+        auto box = make_unique<TBox>(0, 0, 1, 1);
+        auto box_drawable = Drawable::Create(std::move(box), "name_box", "options_for_drawing");
+        ASSERT(box_drawable.get());
+        ASSERT_EQUAL(box_drawable->name, "name_box");
+        ASSERT_EQUAL(box_drawable->draw_options, "options_for_drawing");
+        ASSERT_EQUAL(box_drawable->should_be_drawn, true);
+    }
+    {
+        auto box = make_unique<TBox>(0, 0, 1, 1);
+        auto box_drawable = Drawable::Create(std::move(box), "name_box", "options_for_drawing");
+        ASSERT(box_drawable.get());
+        box_drawable->should_be_drawn = false;
+        ASSERT_EQUAL(box_drawable->name, "name_box");
+        ASSERT_EQUAL(box_drawable->draw_options, "options_for_drawing");
+        ASSERT_EQUAL(box_drawable->should_be_drawn, false);
+    }
+}
+
 void TestDrawableCtor() {
     {
-        Drawable d;
-        TBox box;
+        auto box = new TBox(0, 0, 1, 1);
+        ASSERT_NO_THROW(Drawable::Create(box));
+    }
+    {
+        auto box = new TBox(0, 0, 1, 1);
+        auto box_drawable = Drawable::Create(std::move(box));
+        ASSERT(box_drawable.get());
+        ASSERT_EQUAL(box_drawable->name, "");
+        ASSERT_EQUAL(box_drawable->draw_options, "");
+        ASSERT_EQUAL(box_drawable->should_be_drawn, true);
+    }
+    {
+        auto box = new TBox(0, 0, 1, 1);
+        auto box_drawable = Drawable::Create(std::move(box), "name_box");
+        ASSERT(box_drawable.get());
+        ASSERT_EQUAL(box_drawable->name, "name_box");
+        ASSERT_EQUAL(box_drawable->draw_options, "");
+        ASSERT_EQUAL(box_drawable->should_be_drawn, true);
+    }
+    {
+        auto box = new TBox(0, 0, 1, 1);
+        auto box_drawable = Drawable::Create(std::move(box), "name_box", "options_for_drawing");
+        ASSERT(box_drawable.get());
+        ASSERT_EQUAL(box_drawable->name, "name_box");
+        ASSERT_EQUAL(box_drawable->draw_options, "options_for_drawing");
+        ASSERT_EQUAL(box_drawable->should_be_drawn, true);
+    }
+    {
+        auto box = new TBox(0, 0, 1, 1);
+        auto box_drawable = Drawable::Create(std::move(box), "name_box", "options_for_drawing");
+        ASSERT(box_drawable.get());
+        box_drawable->should_be_drawn = false;
+        ASSERT_EQUAL(box_drawable->name, "name_box");
+        ASSERT_EQUAL(box_drawable->draw_options, "options_for_drawing");
+        ASSERT_EQUAL(box_drawable->should_be_drawn, false);
     }
 }
 
 EFT_IMPLEMENT_TESTFILE(Scene) {
-    EFT_ADD_TEST(TestDrawableCtor,"Scene")
+    EFT_ADD_TEST(TestDrawableCtorFromUniquePtr, "Drawable")
+    EFT_ADD_TEST(TestDrawableCtor,              "Drawable")
 }
 EFT_END_IMPLEMENT_TESTFILE(Scene);
