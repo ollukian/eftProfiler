f9afe57e7b3a7781448d8798e14a9091b59a773f
ollukian <77246672+ollukian@users.noreply.github.com>
2023-01-16 11:20:04 +0100

    add tests for PoiConfig

--- Test/TestPoiConfig.cpp
+++ Test/TestPoiConfig.cpp
@@ -1,3 +1,28 @@
 //
 // Created by Aleksei Lukianchuk on 15-Jan-23.
 //
+
+#include "../Application/NllScans/PoiConfig.h"
+#include "test_runner.h"
+#include "Tester.h"
+#include "EftTests.h"
+
+using namespace eft::stats::scans;
+using namespace std;
+
+void TestReadFromLine()
+{
+    {
+        const string str {"mu_VBF(val 1 2)"};
+        auto poi = PoiConfig::readFromString(str);
+        ASSERT_EQUAL(poi.Name(), "mu_VBF");
+        ASSERT_EQUAL(poi.Value(), 1);
+    }
+}
+
+EFT_IMPLEMENT_TESTFILE(PoiConfig)
+{
+    EFT_ADD_TEST(TestReadFromLine, "PoiConfig");
+}
+EFT_END_IMPLEMENT_TESTFILE(PoiConfig);
+
